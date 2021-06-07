fb4312730138c87a923ca06cd5b9d90523164527
LucaDen <lucadenardo01@icloud.com>
2021-06-07 09:27:14 +0200

    Fine Esercizio

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+xhr.send();
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log("ok");
+    let obj = JSON.parse(xhr.responseText);
+    let tableRow = document.getElementsByTagName("tbody")[0];
+  
+
+for (const user of obj) {
+    let tr = document.createElement("tr");
+    tableRow.appendChild(tr);
+    console.log(user.id);
+
+    let tdName = document.createElement("td");
+    tr.appendChild(tdName);
+    tdName.innerHTML = user.name;
+    
+    let tdUsername = document.createElement("td");
+    tr.appendChild(tdUsername);
+    tdUsername.innerHTML = user.username;
+    
+    let tdEmail = document.createElement("td");
+    tr.appendChild(tdEmail);
+    tdEmail.innerHTML = user.email;
+    
+    let tdPhone = document.createElement("td");
+    tr.appendChild(tdPhone);
+    tdPhone.innerHTML = user.phone;
+    
+    let tdWeb = document.createElement("td");
+    tr.appendChild(tdWeb);
+    tdWeb.innerHTML = user.website;
+   }
+  }
+}
